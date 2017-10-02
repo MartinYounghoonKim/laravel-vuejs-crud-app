bc65e1870b1bed7e5ddb64b406de20bbb39efd91
martin <martin@within.co.kr>
2017-10-03 02:25:53 +0900

    Seperated app header

--- resources/assets/src/common/AppHeader.vue
+++ resources/assets/src/common/AppHeader.vue
@@ -1,7 +1,14 @@
 <template>
 <div class="app-header">
   <h1>CRUD with Laravel &amp; Vuejs</h1>
-    
+  <router-link 
+    to="/Write"
+    class="btn btn-success"
+  >New</router-link>
+  <router-link 
+    to="/"
+    class="btn btn-info"
+  >View List</router-link>
 </div>
 </template>
 
