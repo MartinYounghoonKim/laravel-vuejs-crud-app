668505d3f3441fddc26dd68232079b9c672c1328
martin <martin@within.co.kr>
2017-10-03 02:24:20 +0900

    Edit file name for file name rules

--- resources/assets/src/spa/BoardView/AppContainer.vue
+++ resources/assets/src/spa/BoardView/AppContainer.vue
@@ -30,7 +30,7 @@ import { mapGetters, mapActions } from 'vuex';
 import getApi from '../../api/api_core.js';
 
 export default {
-	name: 'main',
+	name: 'board-view',
   computed: {
 	  ...mapGetters({
 			Contents: 'getContentsData'
@@ -51,56 +51,5 @@ export default {
 </script>
 
 <style lang="scss">
-body {
-	margin: 0;
-	padding: 0;
-}
 
-button {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	background: none;
-	font-size: 100%;
-	vertical-align: baseline;
-	font-family: inherit;
-	font-weight: inherit;
-	color: inherit;
-	-webkit-appearance: none;
-	appearance: none;
-	-webkit-font-smoothing: antialiased;
-	-moz-font-smoothing: antialiased;
-	font-smoothing: antialiased;
-}
-
-body {
-	font: 14px 'Helvetica Neue', Helvetica, Arial, sans-serif;
-	line-height: 1.4em;
-	background: #f5f5f5;
-	color: #4d4d4d;
-	min-width: 500px;
-	max-width: 800px;
-	margin: 0 auto;
-	-webkit-font-smoothing: antialiased;
-	-moz-font-smoothing: antialiased;
-	font-smoothing: antialiased;
-	font-weight: 300;
-}
-
-button,
-input[type="checkbox"] {
-	outline: none;
-}
-
-label{
-	margin:0;
-	padding:0;
-}
-table{
-	thead{
-		th{
-			text-align:center;
-		}
-	}
-}
 </style>
