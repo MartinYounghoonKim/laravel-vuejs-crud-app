70d5dc7125c723ce63b76402840d7b018d4117ca
martin <martin@within.co.kr>
2017-10-03 00:43:22 +0900

    Update api routes about delete

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Students;
+use Illuminate\Http\Response;
 
 class StudentsController extends Controller
 {
@@ -14,5 +15,27 @@ class StudentsController extends Controller
         return response()->json([
             'students' => $students
         ]);
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateStudentRequest $request
+     * @param  int $id
+     * @return Response
+     * UpdateStudentRequest $request, $id
+     */
+    public function show($id)
+    {
+        $student = Students::findOrFail($id);
+        return $student;
+    }
+
+    public function destroy($id)
+    {
+        //$student = Students::findOrFail($id);
+        
+        // $this->studentService->deleteCurrentAvatar($student);
+
+        Students::destroy($id);
     }
 }
