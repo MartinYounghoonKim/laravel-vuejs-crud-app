d341fb65263536ba6e8bb87c1fabbf67fcdac3a0
martin <martin@within.co.kr>
2017-10-02 16:37:15 +0900

    Add module styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
-
-        <!-- Fonts -->
+        <link href="css/app.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     </head>
     <body>
