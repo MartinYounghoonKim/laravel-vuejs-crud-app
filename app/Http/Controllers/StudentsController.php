ae2c69fcea92ee4203dadcbea8c24159bbfa3fdb
martin <martin@within.co.kr>
2017-10-03 18:58:08 +0900

    Update Create route for board

--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -5,17 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Students;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class StudentsController extends Controller
 {
-    public function index()
-    {
-        $students = Students::all();
+  public function index()
+  {
+      $students = Students::all();
 
-        return response()->json([
-            'students' => $students
-        ]);
-    }
+      /*
+      * 하단의 문법으로 해당하는 Id에 해당 하는 쿼리문을 직접 날릴 수도 있음
+      * $students = DB::select('select * from students where id = ?', [100]);
+      */
+
+      return response()->json([
+          'students' => $students
+      ]);
+  }
     /**
      * Update the specified resource in storage.
      *
@@ -29,6 +35,23 @@ class StudentsController extends Controller
         $student = Students::findOrFail($id);
         return $student;
     }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+     public function store(Request $request)
+     {
+      $name = $request->input('name');
+      $birthday = $request->input('birthday');
+      $email = $request->input('email');
+
+      DB::insert('insert into students (name, birth_date, email) values (?, ?, ?)', [$name, $birthday, $email]);
+
+      return response(null, Response::HTTP_CREATED);
+     }
 
     public function destroy($id)
     {
