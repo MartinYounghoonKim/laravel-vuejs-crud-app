8effe9da076c8b2df9f76252a7dc308074f99360
martin <martin@within.co.kr>
2017-10-04 16:11:27 +0900

    Fix Error about style file loading

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv = "Content-Language" content = "en"/>
+        <meta http-equiv = "Content-Type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Laravel</title>
-        <link href="css/app.css" rel="stylesheet" type="text/css">
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     </head>
     <body>
