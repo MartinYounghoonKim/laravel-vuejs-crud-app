e89f0bab857fa91013e281f09e7a2fd690e48fd9
martin <martin@within.co.kr>
2017-10-17 00:06:52 +0900

    Fix students board common header container

---
+++ resources/assets/src/spa/StudentsBoard/AppContainer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app-header">
+        <h1>CRUD with Laravel &amp; Vuejs</h1>
+        <router-link to="/board/create" class="btn btn-success">New</router-link>
+        <router-link to="/board/list" class="btn btn-info">View List</router-link>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'BoardContainer'
+  }
+</script>
+
+<style lang="scss">
+
+</style>
